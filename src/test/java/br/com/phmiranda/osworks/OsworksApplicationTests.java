2568d8c812f1f50f12a889096d17c4fbfc118ad2
phmiranda <pehhagah.1607@gmail.com>
2020-06-19 22:59:31 -0300

    [add] - criação do projeto no Spring Initialzr.

---
+++ src/test/java/br/com/phmiranda/osworks/OsworksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phmiranda.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
